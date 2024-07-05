547fa926e43a4a37a37c07565758cca16bdc5631
Himza25 <103056852+himza25@users.noreply.github.com>
2024-07-05 02:42:55 +0000

    update logs

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,22 +5,23 @@ const User = require('../models/user.model');
 
 // Inscription
 router.post('/signup', async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: 'L\'utilisateur existe déjà.' });
+    console.log('Request body:', req.body); // Ajoutez cette ligne pour voir les données reçues
+    try {
+        const { email, password } = req.body;
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: 'L\'utilisateur existe déjà.' });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({ email, password: hashedPassword });
+        await newUser.save();
+
+        return res.status(201).json({ message: 'Inscription réussie !' });
+    } catch (error) {
+        console.error(error); // Ajouter ce console.log pour voir les erreurs côté serveur
+        return res.status(500).json({ message: 'Erreur serveur.' });
     }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({ email, password: hashedPassword });
-    await newUser.save();
-
-    return res.status(201).json({ message: 'Inscription réussie !' });
-  } catch (error) {
-    console.error(error); // Ajouter ce console.log pour voir les erreurs côté serveur
-    return res.status(500).json({ message: 'Erreur serveur.' });
-  }
 });
 
 // Connexion
